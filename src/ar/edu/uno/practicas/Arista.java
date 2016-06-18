f0af491c0b9cb996d3e0d91c7fd8e4a03b09963e
David Labra Gaona <davidlabragaona@gmail.com>
2016-06-17 22:13:34 -0300

    Se quita la lista de vertices

--- src/ar/edu/uno/practicas/Arista.java
+++ src/ar/edu/uno/practicas/Arista.java
@@ -1,5 +1,42 @@
 package ar.edu.uno.practicas;
+import java.util.*;
 
 public class Arista {
+	private Vertice a;
+	private Vertice b;
+	private Double probabilidad;
+	
+	public Arista(Vertice a, Vertice b) {
+		//Ocultamos los métodos para setear a y b.
+		//Solamente se admite su pasaje al momento de
+		//creación de la arista.
+		this.setA(a); this.setB(b);
+		setProbabilidad(Math.random());
+	}
+
+	//Getters & Setters
+	public Vertice getA() {
+		return a;
+	}
+	
+	private void setA(Vertice a) {
+		this.a = a;
+	}
+	
+	public Vertice getB() {
+		return b;
+	}
+	
+	private void setB(Vertice b) {
+		this.b = b;
+	}
+	
+	public Double getProbabilidad() {
+		return probabilidad;
+	}
+	
+	private void setProbabilidad(Double probabilidad) {
+		this.probabilidad = probabilidad;
+	}
 
 }
